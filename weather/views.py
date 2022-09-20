4535d746032800189665b6f11eaed0365ea70023
Abdulhakim KAYA <73741703+KAYA-99@users.noreply.github.com>
2022-09-20 13:47:07 +0300

    Initial commit

---
+++ weather/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+import json
+import urllib.request
+
+# Create your views here.
+# def index(request):
+#     if request.method == 'POST':
+#         city = request.POST['city']
+#         res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
+#         json_data = json.loads(res)
+#         data = {
+#             "country_code": str(json_data['sys']['country']),
+#             "coordinate": str(json_data['coord']['lon']) + ' ' +
+#             str(json_data['coord']['lat']),
+#             "temp": str(json_data['main']['temp'])+'k',
+#             "pressure": str(json_data['main']['pressure']),
+#             "humidity": str(json_data['main']['humidity']),
+#         }
+#
+#     else:
+#         city = ''
+#         data = {}
+#     return render(request, 'index.html', {'city': city, 'data': data})
+
+
+
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        res = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=cb771e45ac79a4e8e2205c0ce66ff633').read()
+        json_data = json.loads(res)
+        celcius = json_data['main']['temp'] - 273.15
+
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp": str(round(celcius, 2)) + ' ℃',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+    else:
+        city = ''
+        data = {}
+
+    context = {'city': city, 'data': data}
+    return render(request, 'index.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
